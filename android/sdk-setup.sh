43728cec3317a568563fb9bf120b3e90ff5952af
Alexander Sokol <sokol.a@abcloudz.com>
2021-08-18 17:16:34 +0300

    update android sdk setup script

--- android/sdk-setup.sh
+++ android/sdk-setup.sh
@@ -1,3 +1,14 @@
 ANDROID_HOME='/opt/android-sdk-linux'
 CMD_TOOLS='/opt/android-cmdline-tools'
 NDK_BUNDLE='${ANDROID_HOME}/ndk-bundle'
+
+cd /opt
+
+echo "Downloading cmdline-tools"
+mkdir -p ${CMD_TOOLS} \
+    && curl -sSL https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip -o cmdline-tools.zip \
+    && bsdtar xvf cmdline-tools.zip --strip-components=1 -C ${CMD_TOOLS} \
+    && rm cmdline-tools.zip
+
+echo "Downloading android-sdk-linux"
+mkdir -p ${ANDROID_HOME} \
